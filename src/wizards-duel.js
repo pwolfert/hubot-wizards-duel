2ca9012021722468581435ea681ae3dfce8ee380
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-11-04 09:20:09 -0700

    Added more tests and got rid of the note deprecation warning

--- src/wizards-duel.js
+++ src/wizards-duel.js
@@ -1,3 +1,12 @@
+// Description:
+//   Allows hubot to facilitate wizards' duels
+//
+// Commands:
+//   I challenge @<username> to a wizards duel! - Challenges another user to a duel
+//
+// Author:
+//   pwolfert
+//
 
 var effects = require('./wizards-duel/effects.js');
 var spells  = require('./wizards-duel/spells.js');
--- test/wizards-duel-test.js
+++ test/wizards-duel-test.js
@@ -7,20 +7,22 @@ var helper = new Helper('../src/wizards-duel.js');
 
 
 describe('wizards-duel', function() {
+	var message1 = 'I challenge @bob to a wizards duel!';
+	var message2 = 'I accept @alice\'s challenge';
+	var attackMessage1 = 'madefio!';
 
-	describe('Challenge Sending', function() {
-		var message1 = 'I challenge @bob to a wizards duel!';
+	describe('Sending a Challenge', function() {
 
 		before(function() {
 			room = helper.createRoom();
-			return room.user.say('alice', message1).then();
+			room.user.say('alice', message1).then();
 		});
 
 		after(function() {
 			room.destroy();
 		});
 
-		it('responds to challenges', function() {
+		it('responds to challenges - messages', function() {
 			var expectedResult = [
 				['alice', message1],
 				['hubot', [
@@ -37,6 +39,56 @@ describe('wizards-duel', function() {
 
 			expect(room.robot.brain.data._private[duelKey]).to.eql(Manager.STATUS_CHALLENGE_SENT);
 		});
+
+	});
+
+	describe('Accepting a Challenge', function() {
+		var turnData;
+
+		before(function(done) {
+			room = helper.createRoom();
+			room.user.say('alice', message1).then(function() {
+				room.user.say('bob', message2).then(function() {
+					var turnKey = Manager.getTurnKey('alice', 'bob');
+					turnData = room.robot.brain.data._private[turnKey];
+					done();
+				});
+			});
+		});
+
+		after(function() {
+			room.destroy();
+		});
+
+		it('handles challenge accepting - stores turn info', function() {
+			expect(turnData.attack).to.eql(true);
+		});
+
+		it('handles challenge accepting - messages', function() {
+			var startingPlayer = turnData.player;
+			var expectedLast2Messages = [
+				['bob', message2],
+				['hubot', [
+					'*Hear ye! Hear ye!*',
+					'A duel shall now commence between @alice and @bob!' +
+					'@' + startingPlayer + ', thou hast won the coin toss and mayst begin first with an attack.  ' +
+					'Whosoever requireth the list of rules shouldst only type "dueling rules".'
+				].join('\n')]
+			];
+
+			var last2Messages = [
+				room.messages[room.messages.length - 2],
+				room.messages[room.messages.length - 1]
+			];
+			expect(last2Messages).to.deep.eql(expectedLast2Messages);
+		});
+
+		it('handles challenge accepting - stores status', function() {
+			var duelKey = Manager.getDuelKey('alice', 'bob');
+
+			expect(room.robot.brain.data._private[duelKey]).to.eql(Manager.STATUS_DUELING);
+		});
+
 	});
 
 });
