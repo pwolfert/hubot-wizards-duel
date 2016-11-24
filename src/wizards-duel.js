090c94bac2dafed28af2c8716101e7543f332000
Patrick Wolfert <patrickrwolfert@gmail.com>
2016-11-23 19:43:30 -0800

    I'm not sure if I want to abstract the robot stuff out of it.  I want the listening code to be in the manager even if the response object isn't the same.  I think if we want to have other wrappers, they should just emulate the functionality of the robot and the response objects.

--- src/wizards-duel.js
+++ src/wizards-duel.js
@@ -13,15 +13,19 @@ import Manager from './wizards-duel/manager';
 class HubotWrapper {
 
 	constructor(robot) {
-		this.robot
-		this.output = {
-
-		}
-		this.manager = new Manager()
+		this.robot = robot;
+		this.database = robot.brain;
+		this.manager = new Manager(database, () => {
+			return this.output;
+		});
+		this.outputBuffers = [];
 	}
 
 	get output() {
-
+		if (this.outputBuffers.length)
+			return this.outputBuffers[this.outputBuffers.length - 1];
+		else
+			throw new Error('No output buffer has been started.');
 	}
 
 }
